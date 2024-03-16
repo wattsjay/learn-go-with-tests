0a7f1b805969fc1391229f4795668ff8d4feb7bc
Jay Watts <jaywatts618@gmail.com>
2024-03-16 13:21:11 +0200

    Implement integers

---
+++ 2_integers/adder_test.go
@@ -0,0 +1,22 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	// Using d instead of q as we want integers to be printed
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}

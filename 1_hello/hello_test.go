c08f161b28b0589e983e455bdf8aa722e7cadbcd
Jay Watts <jaywatts618@gmail.com>
2024-03-16 12:59:44 +0200

    Refactor hello

--- 1_hello/hello_test.go
+++ 1_hello/hello_test.go
@@ -3,9 +3,33 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+}
 
+func assertCorrectMessage(t testing.TB, got, want string) {
+	// Required to tell the test suite that this method is
+	// a helper. When the test fails the line number will
+	// be reported in the caller instead of here.
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
